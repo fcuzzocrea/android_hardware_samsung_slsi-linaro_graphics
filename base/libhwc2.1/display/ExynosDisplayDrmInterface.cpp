275553c8838763b24e61a523c4478e31b31efea6
Francescodario Cuzzocrea <bosconovic@gmail.com>
2024-03-03 12:05:53 +0100

    libhwc2.1: invalidate the last frame before changing display mode
    * Fix a weird frame mismatch superposing when changing refresh rate or resolution settings
    * For DRM devices create a dummy display since the function signature forces us to have a pointer as param_1
    
    Co-Authored-by: ExtremeXT <extremextdev@gmail.com>
    Change-Id: I71aa6ac0d6d610c0da9965000f145eac6ca19e71
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- base/libhwc2.1/display/ExynosDisplayDrmInterface.cpp
+++ base/libhwc2.1/display/ExynosDisplayDrmInterface.cpp
@@ -411,12 +411,14 @@ int32_t ExynosDisplayDrmInterface::chosePreferredConfig() {
     uint32_t num_configs = 0;
     std::map<uint32_t, displayConfigs_t> temp;
     int32_t err = getDisplayConfigs(&num_configs, NULL, temp);
+    DisplayIdentifier dummyId = {};
+    ExynosDisplay dummyDisplay(dummyId);
     if (err != HWC2_ERROR_NONE || !num_configs)
         return err;
 
     mPreferredModeId = mDrmConnector->get_preferred_mode_id();
     ALOGI("Preferred mode id: %d", mPreferredModeId);
-    err = setActiveConfig(mPreferredModeId, temp[mPreferredModeId]);
+    err = setActiveConfig(dummyDisplay, mPreferredModeId, temp[mPreferredModeId]);
     return err;
 }
 
@@ -642,7 +644,7 @@ void ExynosDisplayDrmInterface::getDisplayHWInfo(uint32_t __unused &xres,
 }
 
 int32_t ExynosDisplayDrmInterface::setActiveConfigWithConstraints(
-    hwc2_config_t config, displayConfigs_t &displayConfig, bool test) {
+    ExynosDisplay __unused &exynosDisplay, hwc2_config_t config, displayConfigs_t &displayConfig, bool test) {
     HDEBUGLOGD(eDebugDisplayConfig, "%s:: %s config(%d)", __func__,
                mDisplayIdentifier.name.c_str(), config);
 
@@ -815,7 +817,8 @@ int32_t ExynosDisplayDrmInterface::clearActiveDrmMode() {
     return ret;
 }
 
-int32_t ExynosDisplayDrmInterface::setActiveConfig(hwc2_config_t config,
+int32_t ExynosDisplayDrmInterface::setActiveConfig(ExynosDisplay __unused &exynosDisplay,
+                                                   hwc2_config_t config,
                                                    displayConfigs_t __unused &displayConfig) {
     ALOGD("%s:: %s config(%d)", __func__, mDisplayIdentifier.name.c_str(), config);
     auto mode = std::find_if(mDrmConnector->modes().begin(), mDrmConnector->modes().end(),
