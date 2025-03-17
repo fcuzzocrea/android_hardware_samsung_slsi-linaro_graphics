072f9d483c827f02a07e7294d399a8e378c9799c
Thomas Turner <thomas_turner@talktalk.net>
2025-03-17 19:09:49 +0000

    exynos3830: Fix decon struct mismatches
    The structs are different to that specified in the 850
    kernel.
    
    Change-Id: If763918495d308a1155c7f0cf3cb3fc7991d678c

--- base/libhwc2.1/platform/exynos3830/DeconHeader.h
+++ base/libhwc2.1/platform/exynos3830/DeconHeader.h
@@ -117,10 +117,17 @@ struct decon_win_config {
   bool protection;
   bool compression;
 };
+struct decon_win_config_extra {
+    int remained_frames;
+    u32 reserved[7];
+};
 struct decon_win_config_data {
   int present_fence;
   int fd_odma;
+  /* vrefresh rate */
+  u32 fps;
   struct decon_win_config config[MAX_DECON_WIN + 2];
+  struct decon_win_config_extra extra;
 };
 struct decon_disp_info {
   enum hwc_ver ver;
