540b60a427b4ac02eefd71b2faf0ba7a72bf2572
Rocky7842 <eric.rocky7842@gmail.com>
2025-09-22 16:37:31 +0300

    fixup! libhwc2.1: invalidate the last frame before changing display mode
    * In the original commit I only added it to S3CFB_WIN_CONFIG, missing devices using EXYNOS_SET_DISPLAY_MODE.
    * This also explains why I never encountered the error fixed by 451008 on my device.
    
    Change-Id: Ic69ee299a391ea062821a3aeb2bee0670628a4ea

--- base/libhwc2.1/display/ExynosDisplayFbInterface.cpp
+++ base/libhwc2.1/display/ExynosDisplayFbInterface.cpp
@@ -204,6 +204,8 @@ int32_t ExynosDisplayFbInterface::setActiveConfig(ExynosDisplay &exynosDisplay,
 
     if (ret < 0) {
         ALOGE("%s EXYNOS_SET_DISPLAY_MODE failed errno : %d, ret: %d", __func__, errno, ret);
+    } else {
+        exynosDisplay.invalidate();
     }
 #endif
 
