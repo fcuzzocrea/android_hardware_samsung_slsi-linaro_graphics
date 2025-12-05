d6dda8dc849013b63df3c74484d6fee43e30bf7c
ExtremeXT <extremextdev@gmail.com>
2025-12-05 13:30:58 +0200

    libhwc2.1: exynos9830: Set DECON_WIN_STATE_FINGERPRINT
    Kernel location: drivers/video/fbdev/exynos/dpu30/decon.h
    
    Change-Id: If7fddbd2ce42fbd5b6d191cf1136679b45ba2412
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>
    (cherry picked from commit c001c052786d4253f258feb790b1fe7b28bd4e71)

--- base/libhwc2.1/platform/exynos9830/DeconHeader.h
+++ base/libhwc2.1/platform/exynos9830/DeconHeader.h
@@ -97,6 +97,7 @@ struct decon_win_config {
     DECON_WIN_STATE_UPDATE,
     DECON_WIN_STATE_CURSOR,
     DECON_WIN_STATE_MRESOL = 0x10000,
+    DECON_WIN_STATE_FINGERPRINT = 0x30000,
   } state;
   union {
     __u32 color;
